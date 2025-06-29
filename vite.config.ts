b51b090c075fbf16551c160016a8cf06374a3778
kitotake <76629813+kitotake@users.noreply.github.com>
2025-06-29 21:42:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // ✅ Vercel attend ce dossier
   }
 })
